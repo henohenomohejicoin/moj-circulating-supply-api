c753b22511af35ce0ff1409790a4506d9ea1b0cb
へのへのもへじ <henohenomoheji.moj@gmail.com>
2026-09-10 01:36:10 +0900

    Increase limit for getSignaturesForAddress to 100

--- api/buy-monitor.js
+++ api/buy-monitor.js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
         params: [
           mint,
           {
-            limit: 10,
+            limit: 100,
             commitment: "confirmed"
           }
         ]
